198e49761b9a822d8c5c2df5611479e2bfabe55c
amsay0609-cloud <am.say0609@gmail.com>
2026-05-10 10:05:21 +0530

    app.py

--- app.py
+++ app.py
@@ -209,6 +209,8 @@ if fetch_clicked:
                     "quiet": True,
                     "no_warnings": True,
                     "cookiefile": "cookies.txt",  # ADD THIS LINE
+                    user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "referer": "https://www.google.com/",
                 }
 
                 status.write("Fetching highest quality stream")
